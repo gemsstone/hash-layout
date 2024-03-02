24c0ee77a2bde5cce77c9810ff0b6029befda552
manowarhashmi <hmdmanowar@gmail.com>
2024-03-02 23:24:51 +0530

    index.tsx updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,1 @@
-import React, { useState } from 'react';
-
-const App = () => {
-  const [count, setCount] = useState(0);
-
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={increment}>Increment</button>
-    </div>
-  );
-};
-
-export default App;
+export { default as Button } from './Components/ActionComponent/Button';
