0d7589354eb1125ebbf978540a3f70c5573908ac
manowarhashmi <hmdmanowar@gmail.com>
2024-03-03 13:21:06 +0530

    previous update

--- src/Components/ActionComponent/Button.tsx
+++ src/Components/ActionComponent/Button.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import '../ActionComponent/style.css'
 
 interface ButtonProps {
     title: string;
     btnClass: string;
+    variant: 'primary' | 'secondary' | 'outlined';
 }
 
-const Button: React.FC<ButtonProps> = ({ title, btnClass }) => {
+const Button: React.FC<ButtonProps> = ({ title, btnClass, variant }) => {
     const staticClass = "h-btn";
     return (
-        <button className={`${btnClass} ${staticClass}`}>
+        <button className={`${staticClass} ${variant}`}>
             {title}
         </button>
     );
