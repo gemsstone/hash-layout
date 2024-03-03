238e89e6b112145d91c23cf876d4e4d5884ae8df
manowarhashmi <hmdmanowar@gmail.com>
2024-03-03 13:47:22 +0530

    align items added

--- src/Components/ActionComponent/Button.tsx
+++ src/Components/ActionComponent/Button.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
+import '../../Styles/ActionComponent.css'
 
 interface ButtonProps {
     title: string;
     btnClass: string;
     variant: 'h-primary-btn' | 'h-secondary-btn' | 'h-outlined-btn';
+    alignPositions: 'h-align-default' | 'h-align-center' | 'h-align-right';
 }
 
-const Button: React.FC<ButtonProps> = ({ title, btnClass, variant }) => {
+const Button: React.FC<ButtonProps> = ({ title, btnClass, variant, alignPositions }) => {
     const staticClass = "h-btn";
     return (
-        <button className={`${staticClass} ${variant}`}>
-            {title}
-        </button>
+        <div className=''>
+            <button className={`${staticClass} ${btnClass} ${variant} ${alignPositions}`}>
+                {title}
+            </button>
+        </div>
+
     );
 };
 
