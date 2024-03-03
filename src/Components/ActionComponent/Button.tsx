f12cdd4598773ba55a28089d406842768d6af285
manowarhashmi <hmdmanowar@gmail.com>
2024-03-03 13:54:52 +0530

    updates

--- src/Components/ActionComponent/Button.tsx
+++ src/Components/ActionComponent/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface ButtonProps {
     title: string;
-    btnClass: string;
+    btnClass: any;
     variant: 'h-primary-btn' | 'h-secondary-btn' | 'h-outlined-btn';
     alignPositions: 'h-align-default' | 'h-align-center' | 'h-align-right';
 }
@@ -10,8 +10,8 @@ interface ButtonProps {
 const Button: React.FC<ButtonProps> = ({ title, btnClass, variant, alignPositions }) => {
     const staticClass = "h-btn";
     return (
-        <div className=''>
-            <button className={`${staticClass} ${btnClass} ${variant} ${alignPositions}`}>
+        <div className={`${alignPositions}`}>
+            <button className={`${staticClass} ${btnClass} ${variant}`}>
                 {title}
             </button>
         </div>
