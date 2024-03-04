a6eb8e42a1272f42f7bbc8f64ec8b990facd4801
Md Manowar Hashmi <42206806+gemsstone@users.noreply.github.com>
2024-03-04 10:36:44 +0530

    Update Button.tsx

--- src/Components/ActionComponent/Button.tsx
+++ src/Components/ActionComponent/Button.tsx
@@ -2,20 +2,34 @@ import React from 'react';
 
 interface ButtonProps {
     title: string;
-    btnClass: any;
+    onClick: () => void;
+    className: string;
     variant: 'h-primary-btn' | 'h-secondary-btn' | 'h-outlined-btn';
     alignPositions: 'h-align-default' | 'h-align-center' | 'h-align-right';
+    disabled: boolean;
+    type?: 'button' | 'submit' | 'reset'; // Add type prop
+    style?: React.CSSProperties; // Add style prop
+    ariaLabel?: string; // Add ariaLabel prop
+    ariaDisabled?: boolean; // Add ariaDisabled prop
 }
 
-const Button: React.FC<ButtonProps> = ({ title, btnClass, variant, alignPositions }) => {
+const Button: React.FC<ButtonProps> = ({ title, onClick, className, variant, alignPositions, disabled, type, style, ariaLabel, ariaDisabled, ...otherProps }) => {
     const staticClass = "h-btn";
     return (
         <div className={`${alignPositions}`}>
-            <button className={`${staticClass} ${btnClass} ${variant}`}>
+            <button
+                className={`${staticClass} ${className} ${variant}`}
+                onClick={onClick}
+                disabled={disabled}
+                type={type}
+                style={style}
+                aria-label={ariaLabel}
+                aria-disabled={ariaDisabled}
+                {...otherProps}
+            >
                 {title}
             </button>
         </div>
-
     );
 };
 
