b77d655292c9b769db9c3dd07ccbafaa27e07faa
Md Manowar Hashmi <42206806+gemsstone@users.noreply.github.com>
2024-03-04 12:12:52 +0530

    Update Button.tsx

--- src/Components/ActionComponent/Button.tsx
+++ src/Components/ActionComponent/Button.tsx
@@ -1,36 +1,38 @@
 import React from 'react';
 
+// Define the props interface for the Button component
 interface ButtonProps {
-    title: string;
-    onClick: () => void;
-    className: string;
-    variant: 'h-primary-btn' | 'h-secondary-btn' | 'h-outlined-btn';
-    alignPositions: 'h-align-default' | 'h-align-center' | 'h-align-right';
-    disabled: boolean;
-    type?: 'button' | 'submit' | 'reset'; // Add type prop
-    style?: React.CSSProperties; // Add style prop
-    ariaLabel?: string; // Add ariaLabel prop
-    ariaDisabled?: boolean; // Add ariaDisabled prop
+    title: string; // The text displayed on the button
+    onClick: () => void; // The function to call when the button is clicked
+    className: string; // Additional CSS classes for styling
+    variant: 'h-primary-btn' | 'h-secondary-btn' | 'h-outlined-btn'; // The variant of the button
+    alignPositions: 'h-align-default' | 'h-align-center' | 'h-align-right'; // The alignment of the button
+    disabled: boolean; // Whether the button is disabled
+    type?: 'button' | 'submit' | 'reset'; // The type of the button
+    style?: React.CSSProperties; // Custom styles for the button
+    ariaLabel?: string; // Aria label for accessibility
+    ariaDisabled?: boolean; // Aria disabled state for accessibility
 }
 
+// Define the Button component
 const Button: React.FC<ButtonProps> = ({ title, onClick, className, variant, alignPositions, disabled, type, style, ariaLabel, ariaDisabled, ...otherProps }) => {
-    const staticClass = "h-btn";
+    const staticClass = "h-btn"; // Static class for styling
     return (
         <div className={`${alignPositions}`}>
             <button
-                className={`${staticClass} ${className} ${variant}`}
-                onClick={onClick}
-                disabled={disabled}
-                type={type}
-                style={style}
-                aria-label={ariaLabel}
-                aria-disabled={ariaDisabled}
-                {...otherProps}
+                className={`${staticClass} ${className} ${variant}`} // Combine static and dynamic classes
+                onClick={onClick} // Attach onClick event handler
+                disabled={disabled} // Set disabled state
+                type={type} // Set button type
+                style={style} // Apply custom styles
+                aria-label={ariaLabel} // Set aria label
+                aria-disabled={ariaDisabled} // Set aria disabled state
+                {...otherProps} // Pass any other props to the button
             >
-                {title}
+                {title} // Display button text
             </button>
         </div>
     );
 };
 
-export default Button;
+export default Button; // Export the Button component
