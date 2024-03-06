c02fd7609dd7dba43004a929da30d0d41624a53a
Md Manowar Hashmi <42206806+gemsstone@users.noreply.github.com>
2024-03-06 16:15:29 +0530

    Update RadioButton.tsx

--- src/Components/ActionComponent/RadioButton.tsx
+++ src/Components/ActionComponent/RadioButton.tsx
@@ -1,1 +1,32 @@
 
+import React, { Component, ChangeEvent } from 'react';
+
+interface RadioButtonProps {
+  label: string;
+  value: string;
+  checked: boolean;
+  onChange: (value: string) => void;
+}
+
+class RadioButton extends Component<RadioButtonProps> {
+  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onChange, value } = this.props;
+    onChange(value);
+  };
+
+  render() {
+    const { label, checked } = this.props;
+    return (
+      <label>
+        <input
+          type="radio"
+          checked={checked}
+          onChange={this.handleChange}
+        />
+        {label}
+      </label>
+    );
+  }
+}
+
+export default RadioButton;
