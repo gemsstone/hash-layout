c728ad56c56073d37a512bb91bf6b01ee44c74c4
Md Manowar Hashmi <42206806+gemsstone@users.noreply.github.com>
2024-03-06 16:13:35 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,3 @@
 export { default as Button } from './Components/ActionComponent/Button';
 export { default as Autocomplete } from './Components/ActionComponent/Autocomplete';
+export { default as Checkbox } from './Components/ActionComponent/Checkbox';
