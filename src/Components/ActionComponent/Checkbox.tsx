43e3a065341fe167069381c69257f6d3681dfa2e
Md Manowar Hashmi <42206806+gemsstone@users.noreply.github.com>
2024-03-06 16:11:15 +0530

    Update Checkbox.tsx

--- src/Components/ActionComponent/Checkbox.tsx
+++ src/Components/ActionComponent/Checkbox.tsx
@@ -1,1 +1,47 @@
 
+import React, { Component } from 'react';
+
+interface CheckboxProps {
+  label: string;
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}
+
+interface CheckboxState {
+  checked: boolean;
+}
+
+class Checkbox extends Component<CheckboxProps, CheckboxState> {
+  constructor(props: CheckboxProps) {
+    super(props);
+    this.state = {
+      checked: props.checked || false,
+    };
+  }
+
+  handleChange = () => {
+    const { onChange } = this.props;
+    const { checked } = this.state;
+    const newChecked = !checked;
+
+    this.setState({ checked: newChecked });
+
+    if (onChange) {
+      onChange(newChecked);
+    }
+  };
+
+  render() {
+    const { label } = this.props;
+    const { checked } = this.state;
+
+    return (
+      <label>
+        <input type="checkbox" checked={checked} onChange={this.handleChange} />
+        {label}
+      </label>
+    );
+  }
+}
+
+export default Checkbox;
